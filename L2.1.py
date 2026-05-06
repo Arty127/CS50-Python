7d04bcf8bf672ff688a11b87dc7448038c193724
Your Name <you@example.com>
2026-05-06 22:35:17 +0500

    getting all loopy

---
+++ L2.1.py
@@ -0,0 +1,18 @@
+# CAT DOT PYTHON
+# MMM EEE OOO WWW :)
+
+
+while True:
+    n = int(input("what is n?"))
+    if n>0:
+        break
+
+
+
+for _ in range(n):
+    print("meow")
+
+    
+
+
+
---
+++ L2.py
@@ -0,0 +1,26 @@
+#loops and stuff a->b->c->d->a
+
+# M E O W
+
+#while loop:
+i = 0
+
+while i<3:
+    print("meow")
+    i+=1
+
+
+#lists - data types/structs:
+#   [0, 1, 2, 3, 4]
+# we don't have to explicitly name variables as and can use
+# underscores "_" instead as a placeholder for these
+for _ in range(1):
+    print("meeoww")
+
+
+#python can print multiple strings just by using the
+# multiplication operator
+print("meow\n"*3, end ="")
+
+
+
